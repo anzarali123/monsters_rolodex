4d7a15792bffb06e844a97c3342e258651c36aad
AnzarAli <anzarali150799@gmail.com>
2022-03-15 20:33:29 +0530

    fetched the data from an api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField:''
+    }
+  }
+
+  onSearchChange = event => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(()=> {
+      return {searchField}
+    });
+  }
+  
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => {
+      this.setState(() => {
+        return {monsters:users}
+      })
+    })
+  }
+  
+
+  render() {
+    const {monsters,searchField} = this.state;
+    const {onSearchChange} = this;
+
+    const filteredMonsters = monsters.filter(monster => {
+      return monster.name.toLowerCase().includes(searchField);
+    })
+    return (
+      <div>
+      <input type="search" placeholder='Search monsters...' className='search-box' 
+       onChange={onSearchChange}
+       />
+      {
+        filteredMonsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
+      }
+      </div>
+    )
+  }
 }
-
 export default App;
